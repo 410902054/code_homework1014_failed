e419b54373babfb1e44bf75c492e3f196afcaecd
410902054 <71751283+410902054@users.noreply.github.com>
2020-10-28 12:07:21 +0800

    change to a long  block

--- main.ts
+++ main.ts
@@ -1,14 +1,31 @@
-let X = 4
 let Y = 4
-for (let X = 0; X <= 4; X++) {
-    for (let Y = 0; Y <= 4; Y++) {
-        if (X == 4) {
-            led.plot(X, Y)
-        } else if (false) {
-        	
-        } else {
-        	
-        }
-        basic.pause(100)
-    }
+let X = 4
+for (let index = 0; index < 5; index++) {
+    led.plot(X, Y)
+    basic.pause(100)
+    Y += -1
+}
+X = 3
+for (let index = 0; index < 5; index++) {
+    led.plot(X, Y + 1)
+    basic.pause(100)
+    Y += 1
+}
+X = 2
+for (let index = 0; index < 5; index++) {
+    led.plot(X, Y)
+    basic.pause(100)
+    Y += -1
+}
+X = 1
+for (let index = 0; index < 5; index++) {
+    led.plot(X, Y + 1)
+    basic.pause(100)
+    Y += 1
+}
+X = 0
+for (let index = 0; index < 5; index++) {
+    led.plot(X, Y)
+    basic.pause(100)
+    Y += -1
 }
